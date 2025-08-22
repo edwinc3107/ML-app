11134960a64f0df4bacc11f08c85eb8b78550a05
Edwin <officialedwn@gmail.com>
2025-08-21 22:19:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🐧 Club Penguin - experiment with ML')
 
-st.info('Play around with our factor drivers, to see cool visuals!')
+st.info('Play around with our factor drivers, to see cool visuals')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+df
